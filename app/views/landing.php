b7ed15fe610e76f998b8b3131ccc529c557e21ed
dionisio202 <solisedison@outlook.com>
2024-10-26 09:46:24 -0500

    added styles

---
+++ app/views/landing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Junta Administradora de Agua Potable</title>
+    <link rel="stylesheet" href="../public/styles/landpage.css">
+</head>
+<body>
+    <header>
+        <h1>JUNTA ADMINISTRADORA DE AGUA POTABLE</h1>
+        <h2>"SAN VICENTE YACULOMA Y BELLAVISTA EL ROSARIO"</h2>
+        <nav>
+            <a href="#">Facturación</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Tickets</a>
+            <a href="#">Contacto</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+    <section>
+        <p>Bienvenidos a mi página web, espero que les guste amiguitos, bendiciones.</p>
+        <p>Spoiler: no acabamos</p>
+        <p>Spoiler2: ¿les gusta?</p>
+    </section>
+</body>
+</html>
