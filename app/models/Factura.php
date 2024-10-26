8ac3089bd0dd9150a0e70099b258623ff3d0b2f9
YasuoArcher <marlon25.masabanda@gmail.com>
2024-10-26 11:09:28 -0500

    Rutas corregidas

--- app/models/Factura.php
+++ app/models/Factura.php
@@ -2,24 +2,12 @@
 // app/models/Factura.php
 
 class Factura {
+
     public static function getAll() {
         // Conexión a la base de datos y consulta para obtener todas las facturas
-        $db = new PDO('mysql:host=localhost;dbname=junta_agua', 'user', 'password');
-        $stmt = $db->query('SELECT * FROM facturas');
+        $db = new PDO('mysql:host=localhost;dbname=junta_agua', 'root', '');
+        $stmt = $db->query('SELECT * FROM usuario');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function create($data) {
-        // Conexión y consulta para crear una nueva factura
-        $db = new PDO('mysql:host=localhost;dbname=junta_agua', 'user', 'password');
-        $stmt = $db->prepare('INSERT INTO facturas (nombre, cedula, telefono, detalle) VALUES (?, ?, ?, ?)');
-        $stmt->execute([$data['nombre'], $data['cedula'], $data['telefono'], $data['detalle']]);
-    }
-
-    public static function delete($id) {
-        // Conexión y consulta para eliminar una factura
-        $db = new PDO('mysql:host=localhost;dbname=junta_agua', 'user', 'password');
-        $stmt = $db->prepare('DELETE FROM facturas WHERE id = ?');
-        $stmt->execute([$id]);
-    }
 }
--- public/index.php
+++ public/index.php
@@ -1,20 +1,11 @@
 <?php
-require_once '../app/controllers/FacturaController.php';
+require_once __DIR__. '/../app/controllers/FacturaController.php';
 
 $controller = new FacturaController();
 
 $action = $_GET['action'] ?? 'index';
 
 switch ($action) {
-    case 'create':
-        $controller->create();
-        break;
-    case 'store':
-        $controller->store($_POST);
-        break;
-    case 'delete':
-        $controller->delete($_GET['id']);
-        break;
     default:
         $controller->index();
         break;
