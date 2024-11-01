c1c4fc712f0f51d490109237f1889b73d337d18c
Jairo Freire <jairofreireortiz10@gmail.com>
2024-11-01 13:40:52 -0500

    Invoice payment interface added

---
+++ app/views/pagos.php
@@ -0,0 +1,72 @@
+<div class="payment-container">
+    <h1>Nueva Factura</h1>
+    <div class="user-info">
+        <div class="user-header">
+            <div class="user-log-details">
+                <div class="user-logo">User Logo</div>
+                <div class="user-details">
+                    <span>Christopher Santamaria</span>
+                </div>
+            </div>
+            <div class="invoice-number">
+                <label>No.</label>
+                <input type="text" value="1" readonly class="input-payment">
+            </div>
+        </div>
+        <div class="user-dates">
+            <div class="dates-user-details">
+                <div class="user-dates-id">
+                    <label for="user">Cedula</label>
+                    <input type="text" class="input-payment">
+                </div>
+                <div class="user-dates-impuest">
+                    <label for="user">ID impuesto</label>
+                    <input type="text" class="input-payment">
+                </div>
+                <div class="user-dates-phone">
+                    <label for="user">Telefono</label>
+                    <input type="text" class="input-payment">
+                </div>
+            </div>
+            <div class="invoice-details">
+                <div class="invoice-details-main">
+
+                    <div class="invoice-date-one">
+                        <label>Fecha:</label>
+                        <input type="date" value="2020-07-01" class="input-payment">
+                    </div>
+                    <div class="invoice-date-two">
+                        <label>Fecha de Emisión:</label>
+                        <input type="date" value="2020-07-01" class="input-payment">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Cédula</th>
+                    <th>Nombres</th>
+                    <th>Medidor</th>
+                    <th>Fecha de Cobro</th>
+                    <th>Detalle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1709510299</td>
+                    <td>Christopher Santamaria</td>
+                    <td>0969019125</td>
+                    <td>Observación</td>
+                    <td><button>+ Ticket</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <button class="back-button">Regresar</button>
+</div>
