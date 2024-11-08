c33b6aea54d423050d334cf40c19b9462191edd6
CriSant20 <92830667+CriSant20@users.noreply.github.com>
2024-11-08 15:27:15 -0500

    agregacion del css para la responsividad

--- app/views/login.php
+++ app/views/login.php
@@ -9,20 +9,29 @@
 
 <body>
     <header>
-        <!-- Opcional: Puedes añadir elementos de encabezado aquí -->
+    <div class="header-logo">
+            <img src="../img/LOGO.png" alt="j" class="logo">
+        </div>
+        <nav class="header-nav">
+            <ul>
+                
+                <li><a href="#help" class="login-button">Ayuda</a></li>
+            </ul>
+        </nav>
     </header>
 
     <section class="login-section">
         <div class="login-container">
             <div class="login-card">
-                <<img class="logo" alt="logo">
+                <img class="logo" alt="">
                 <h2>Bienvenid@</h2>
                 <h5>Inicie sesión para acceder a los beneficios del sistema</h5>
-                
+                <br>
                 <!-- Formulario de inicio de sesión -->
                 <form action="index.php?action=login" method="post">
                     <input type="text" id="username" name="username" placeholder="Cédula de Identidad" required>
                     <input type="password" id="password" name="password" placeholder="Contraseña" required>
+                    <h5>Recuperar Contraseña</h5>
                     <button type="submit">Iniciar Sesión</button>
                 </form>
                 
