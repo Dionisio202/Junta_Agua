89772b948feca47eff9274b328fccbc969d01cb3
Matt <mateo19dg@gmail.com>
2024-11-16 11:47:31 -0500

    Se envían las correciones de configuracion de base de datos. Conexión remota. Cambios en modelos de factura y usuario. Cambio en vista de factura.

--- app/models/User.php
+++ app/models/User.php
@@ -3,28 +3,28 @@ require_once '../config/database.php';
 
 class User {
     private $conn;
-    private $table_name = "usuario";
+    private $table_name = "usuarios";
 
     public function __construct($db) {
         $this->conn = $db;
     }
 
-    public function createUser($nombre, $password, $rol = 'usuario') {
-        $query = "INSERT INTO " . $this->table_name . " (nombre, password, rol) VALUES (:nombre, :password, :rol)";
+    public function createUser($nombre, $password, $rol = 'Admin') {
+        $query = "INSERT INTO " . $this->table_name . " (nombre, clave, rol) VALUES (:nombre, :clave, :rol)";
         $stmt = $this->conn->prepare($query);
 
         $stmt->bindParam(":nombre", $nombre);
-        $stmt->bindParam(":password", password_hash($password, PASSWORD_DEFAULT));
+        $stmt->bindParam(":clave", password_hash($password, PASSWORD_DEFAULT));
         $stmt->bindParam(":rol", $rol);
 
         return $stmt->execute();
     }
 
     public function authenticateUser($cedula, $password) {
-        $query = "SELECT nombre, rol FROM " . $this->table_name . " WHERE cedula = :cedula AND password = :password LIMIT 1";
+        $query = "SELECT nombre, rol FROM " . $this->table_name . " WHERE cedula = :cedula AND clave = :clave LIMIT 1";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":cedula", $cedula);
-        $stmt->bindParam(":password", $password); // Contraseña sin cifrar
+        $stmt->bindParam(":clave", $password); // Contraseña sin cifrar
         $stmt->execute();
         
         if ($stmt->rowCount() > 0) {
