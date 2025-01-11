3e33c1d5b0a98ac250070d00db6e233f11592dab
Tamimi <tmaliza4821@uta.edu.ec>
2025-01-10 22:31:03 -0500

    Se corrige el color del h2 del sidebar

--- app/views/index.php
+++ app/views/index.php
@@ -8,7 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sidebar</title>
+    <title>Facturaqua</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
